f5a4ad73cb3696878c527e760e79e73194a372e7
conley <743703241@qq.com>
2021-09-02 20:17:56 +0800

    feat(tensorflow-op): join comm feats and parallel for(conley/dev#1)

--- aliccp_rocksdb_op.cpp
+++ aliccp_rocksdb_op.cpp
@@ -1,29 +1,43 @@
+#include "Timer.h"
 #include "comm_feats_generated.h"
 #include "example_generated.h"
 #include "feature_generated.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include <functional>
 #include <rocksdb/db.h>
 #include <rocksdb/options.h>
 #include <rocksdb/table.h>
+#include <unordered_set>
 
 static rocksdb::Status
 open_db(const char* path, rocksdb::DB** db)
 {
     rocksdb::Options opt;
-    opt.create_if_missing = true;
+    opt.create_if_missing = false;
     opt.max_open_files = 3000;
-    opt.write_buffer_size = 500 * 1024 * 1024;
     opt.max_write_buffer_number = 3;
     opt.target_file_size_base = 67108864;
+    opt.new_table_reader_for_compaction_inputs = true;
 
     rocksdb::BlockBasedTableOptions table_opt;
     table_opt.block_cache = rocksdb::NewLRUCache((1024 * 1024 * 1024));
-    table_opt.block_cache_compressed = rocksdb::NewLRUCache(500 * (1024 * 1024));
     opt.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_opt));
 
-    return rocksdb::DB::Open(opt, path, db);
+    return rocksdb::DB::OpenForReadOnly(opt, path, db);
+}
+
+namespace std {
+template<>
+struct hash<rocksdb::Slice>
+{
+    size_t operator()(rocksdb::Slice const& s) const noexcept { return hash_op(s.ToString(true)); }
+
+  private:
+    std::hash<std::string> hash_op;
+};
 }
 
 namespace tensorflow {
@@ -51,7 +65,7 @@ class AliCCPRocksDBOp : public OpKernel
         std::string comm_feats_db;
         OP_REQUIRES_OK(context, context->GetAttr("comm_feats_db", &comm_feats_db));
 
-        OP_REQUIRES_OK(context, context->GetAttr("max_feats", &max_feats));
+        OP_REQUIRES_OK(context, context->GetAttr("max_feats", &max_feats_));
 
         rocksdb::DB* db;
         auto status = open_db(examples_db.c_str(), &db);
@@ -65,6 +79,8 @@ class AliCCPRocksDBOp : public OpKernel
             context->CtxFailure(Status(error::INVALID_ARGUMENT, status.ToString()));
         }
         comm_feats_db_ = std::shared_ptr<rocksdb::DB>(db);
+
+        read_opts_.readahead_size = 1024 * 1024 * 1024;
     }
 
     static Tensor* alloc_tensor(OpKernelContext* context, std::vector<int32> const& dims, int const i)
@@ -86,31 +102,16 @@ class AliCCPRocksDBOp : public OpKernel
         return tensor;
     }
 
-    void Compute(OpKernelContext* context) override
+    void parse_examples(OpKernelContext* context,
+                        std::vector<const aliccp::Example*> const& examples,
+                        std::unordered_map<std::string, const aliccp::CommFeature*> const& comm_feats,
+                        Tensor* field_id_tensor,
+                        Tensor* feat_id_tensor,
+                        Tensor* feats_tensor,
+                        Tensor* y,
+                        Tensor* z,
+                        Tensor* lens_tensor)
     {
-        auto const input = context->input(0);
-
-        if (input.dims() != 1) {
-            context->CtxFailure(Status(error::INVALID_ARGUMENT, "1d tensor is accepted only."));
-            return;
-        }
-
-        std::vector<std::string> buf;
-        read_values(input, buf);
-
-        auto const nelems = static_cast<int32>(input.NumElements());
-
-        auto field_id_tensor = alloc_tensor(context, { nelems, max_feats }, 0);
-        auto feat_id_tensor = alloc_tensor(context, { nelems, max_feats }, 1);
-        auto feats_tensor = alloc_tensor(context, { nelems, max_feats }, 2);
-        auto y = alloc_tensor(context, { nelems }, 3);
-        auto z = alloc_tensor(context, { nelems }, 4);
-        auto lens_tensor = alloc_tensor(context, { nelems }, 5);
-
-        if (!field_id_tensor || !feat_id_tensor || !feats_tensor || !y || !z || !lens_tensor) {
-            return;
-        }
-
         auto feat_matrix = feats_tensor->matrix<float>();
         auto field_id_matrix = field_id_tensor->matrix<int64>();
         auto feat_id_matrix = feat_id_tensor->matrix<int64>();
@@ -127,71 +128,188 @@ class AliCCPRocksDBOp : public OpKernel
             }
         };
 
-        for (auto i = 0; i < buf.size(); ++i) {
-            auto const& data = buf[i];
-            if (data.empty()) {
-                continue;
-            }
+        auto batch_size = 64;
+        auto batch_nums = (examples.size() + batch_size - 1) / batch_size;
 
-            auto example = aliccp::GetExample(data.data());
+        auto parse_batch = [this,
+                            batch_size,
+                            examples,
+                            comm_feats,
+                            &field_id_matrix,
+                            &feat_matrix,
+                            &feat_id_matrix,
+                            &y,
+                            &z,
+                            &lens_tensor,
+                            field_to_int64](Eigen::Index start, Eigen::Index end) {
+            start = std::min(start * batch_size, (Eigen::Index)examples.size());
+            end = std::min(end * batch_size, (Eigen::Index)examples.size());
 
-            y->flat<int64>()(i) = static_cast<int64>(example->y());
-            z->flat<int64>()(i) = static_cast<int64>(example->z());
-
-            auto feats = example->feats();
-            lens_tensor->flat<int64>()(i) = std::min((int32)feats->Length(), max_feats);
-
-            for (auto k = 0; k < max_feats; ++k) {
-                if (k >= feats->Length()) {
-                    feat_matrix(i, k) = 0.0;
-                    field_id_matrix(i, k) = 0;
-                    feat_id_matrix(i, k) = 0;
+            for (auto i = start; i < end; ++i) {
+                auto const example = examples[i];
+                if (!example) {
                     continue;
                 }
 
-                auto feat = feats->Get(k);
-                auto field_id = feat->feat_field_id();
-                auto feat_id = feat->feat_id();
-                auto value = feat->value();
+                y->flat<int64>()(i) = static_cast<int64>(example->y());
+                z->flat<int64>()(i) = static_cast<int64>(example->z());
 
-                feat_matrix(i, k) = value;
-                field_id_matrix(i, k) = field_to_int64(field_id->str());
-                feat_id_matrix(i, k) = feat_id;
+                auto feats = example->feats();
+
+                int k = 0;
+                int len = std::min((int32)feats->Length(), max_feats_);
+                for (; k < len; ++k) {
+                    auto feat = feats->Get(k);
+                    auto field_id = feat->feat_field_id();
+                    auto feat_id = feat->feat_id();
+                    auto value = feat->value();
+
+                    feat_matrix(i, k) = value;
+                    field_id_matrix(i, k) = field_to_int64(field_id->str());
+                    feat_id_matrix(i, k) = feat_id;
+                }
+
+                auto comm_feat_id = example->comm_feat_id();
+                auto iter = comm_feats.find(comm_feat_id->str());
+
+                if (iter != comm_feats.cend()) {
+                    auto const comm_feat = iter->second->feats();
+                    auto cap = std::min(max_feats_, (int32)(comm_feat->Length() + feats->Length()));
+                    for (; k < cap; ++k) {
+                        auto feat = comm_feat->Get(k - len);
+                        auto field_id = feat->feat_field_id();
+                        auto feat_id = feat->feat_id();
+                        auto value = feat->value();
+
+                        feat_matrix(i, k) = value;
+                        field_id_matrix(i, k) = field_to_int64(field_id->str());
+                        feat_id_matrix(i, k) = feat_id;
+                    }
+                }
+
+                lens_tensor->flat<int64>()(i) = k;
+                for (; k < max_feats_; ++k) {
+                    feat_matrix(i, k) = 0.0;
+                    field_id_matrix(i, k) = 0;
+                    feat_id_matrix(i, k) = 0;
+                }
             }
+        };
+
+        auto thread_pool = context->device()->tensorflow_cpu_worker_threads()->workers;
+        thread_pool->ParallelFor(batch_nums, 10 * 600 * batch_size, std::move(parse_batch));
+    }
+
+    void Compute(OpKernelContext* context) override
+    {
+        auto const input = context->input(0);
+
+        if (input.dims() != 1) {
+            context->CtxFailure(Status(error::INVALID_ARGUMENT, "1d tensor is accepted only."));
+            return;
         }
+
+        std::vector<std::string> buf;
+        OP_REQUIRES_OK(context, read_values(input, buf));
+
+        auto const nelems = static_cast<int32>(input.NumElements());
+
+        auto field_id_tensor = alloc_tensor(context, { nelems, max_feats_ }, 0);
+        auto feat_id_tensor = alloc_tensor(context, { nelems, max_feats_ }, 1);
+        auto feats_tensor = alloc_tensor(context, { nelems, max_feats_ }, 2);
+        auto y = alloc_tensor(context, { nelems }, 3);
+        auto z = alloc_tensor(context, { nelems }, 4);
+        auto lens_tensor = alloc_tensor(context, { nelems }, 5);
+
+        if (!field_id_tensor || !feat_id_tensor || !feats_tensor || !y || !z || !lens_tensor) {
+            return;
+        }
+
+        std::vector<const aliccp::Example*> examples;
+        std::transform(buf.cbegin(), buf.cend(), std::back_inserter(examples), [](std::string const& s) {
+            return aliccp::GetExample(s.data());
+        });
+
+        std::unordered_set<rocksdb::Slice> comm_keys;
+        std::transform(examples.cbegin(),
+                       examples.cend(),
+                       std::inserter(comm_keys, comm_keys.end()),
+                       [](const aliccp::Example* example) {
+                           auto key = rocksdb::Slice(example->comm_feat_id()->c_str(),
+                                                     example->comm_feat_id()->Length());
+                           return key;
+                       });
+
+        std::vector<string> comm_feats_buf;
+        std::unordered_map<std::string, const aliccp::CommFeature*> comm_feats;
+        OP_REQUIRES_OK(context,
+                       read_db(comm_feats_db_,
+                               std::vector<rocksdb::Slice>(comm_keys.begin(), comm_keys.end()),
+                               comm_feats_buf));
+
+        std::transform(comm_feats_buf.cbegin(),
+                       comm_feats_buf.cend(),
+                       std::inserter(comm_feats, comm_feats.end()),
+                       [](std::string const& s) {
+                           auto comm_feat = aliccp::GetCommFeature(s.data());
+                           return std::make_pair(comm_feat->comm_feat_id()->str(), comm_feat);
+                       });
+
+        Timer timer;
+        parse_examples(
+            context, examples, comm_feats, field_id_tensor, feat_id_tensor, feats_tensor, y, z, lens_tensor);
+        LOG(INFO) << "parse examples cost " << timer.elapsed_ms() << "ms";
     }
 
   private:
-    void read_values(Tensor const& input, std::vector<std::string>& values)
+    Status read_db(std::shared_ptr<rocksdb::DB> db,
+                   std::vector<rocksdb::Slice> const& keys,
+                   std::vector<std::string>& values)
+    {
+        Timer timer;
+        auto status = db->MultiGet(read_opts_, keys, &values);
+
+        int failed = 0;
+        for (auto i = 0; i < keys.size(); ++i) {
+            auto s = status[i];
+            if (!s.ok()) {
+                failed += 1;
+            }
+        }
+
+        LOG(INFO) << "read keys size = " << keys.size() << ", cost = " << timer.elapsed_ms() << "ms"
+                  << ", failed counts = " << failed;
+        return Status::OK();
+    }
+
+    Status read_values(Tensor const& input, std::vector<std::string>& values)
     {
         auto nelems = input.NumElements();
         auto input_flat = input.flat<int64>();
         std::vector<rocksdb::Slice> keys;
 
-        uint32_t* keybuf = (uint32_t*)::malloc(sizeof(uint32_t)*nelems);
+        uint32_t* keybuf = (uint32_t*)::malloc(sizeof(uint32_t) * nelems);
+        if (!keybuf) {
+            return Status(error::INTERNAL, "malloc buf failed.");
+        }
+
         for (auto i = 0; i < nelems; ++i) {
             auto example_id = static_cast<uint32_t>(input_flat(i));
             keybuf[i] = example_id;
             const char* p = reinterpret_cast<const char*>(keybuf + i);
-
             rocksdb::Slice key(p, sizeof(example_id));
-            LOG(INFO) << "enqueue key " << key.ToString(true);
             keys.push_back(key);
         }
 
-        auto status = example_db_->MultiGet(rocksdb::ReadOptions(), keys, &values);
-        for (auto i = 0; i < input.NumElements(); ++i) {
-            auto s = status[i];
-            LOG(INFO) << "read example_id = " << keybuf[i] << "(hex=" << keys[i].ToString(true) << ") "
-                      << (s.ok() ? "succ" : "failed :") << (s.ok() ? "" : status[i].ToString());
-        }
-
+        auto status = read_db(example_db_, keys, values);
         free(keybuf);
+        return status;
     }
 
     std::shared_ptr<rocksdb::DB> example_db_;
     std::shared_ptr<rocksdb::DB> comm_feats_db_;
-    int32 max_feats;
+    rocksdb::ReadOptions read_opts_;
+    int32 max_feats_;
 };
 REGISTER_KERNEL_BUILDER(Name("AliCCPRocksDB").Device(DEVICE_CPU), AliCCPRocksDBOp);
 };
